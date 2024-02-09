462760a0eb95936ec2085ba13947c600fade65b8
Manuela Silva Souza <ssouza.manuela23@gmail.com>
2024-02-09 10:34:27 -0300

    testando rota

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { Ave } from './model/Ave';
 import { Reptil } from './model/Reptil';
 import { Mamifero } from './model/Mamifero';
+import { Habitat } from './model/Habitat';
+import { Atracao } from './model/Atracao';
 
 const server = express();
 const port = 3000;
@@ -22,7 +24,32 @@ server.post('/ave', (req, res) =>{
   const {nome, idade, genero, envergadura} = req.body;
   let ave: Ave = new Ave(nome, idade, genero, envergadura);
   res.json(["A nova ave do zoologico é:",ave]);
+})
+
+server.post('/habitat', (req, res)=> {
+  const  { nome, animais} = req.body;
+  const habitat = new Habitat(nome, animais);
+  console.log(habitat);
+  res.status(200).json('Habitat criando');
+})
+
+server.post('/atracao', (req, res)=> {
+  const  { nome, habitat} = req.body;
+  const atracao = new Atracao(nome, habitat);
+  console.log(atracao);
+  res.status(200).json('Atracao criada');
+})
+
+
+server.post('/zoologico', (req, res) => {
+  const {nome, atracao}= req.body;
+  const zoo = new Zoologico (nome, atracao);
+  console.log(zoo);
+  res.status(200).json('Zoologico criado');
 });
+
+
 server.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 })
+
